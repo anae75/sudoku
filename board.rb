4af52799dc3b66e47247c1c3c204804ad6ba47f3
Ana <ana@foo.com>
2014-01-17 20:25:59 -0500

    add simple rows and columns as sets of numbers

--- board.rb
+++ board.rb
@@ -1,15 +1,69 @@
 class Board
   def initialize
+    @rows = []
+    @columns = []
+    9.times do
+      @rows << NumberSet.new
+      @columns << NumberSet.new
+    end
   end
 
   def rows
-    return (0..8).to_a
+    return @rows
   end
   def columns
-    return (0..8).to_a
+    return @columns
   end
   def squares
     return (0..8).to_a
   end
 
+  def position(rownum, columnum)
+    return Position.new(self, row(rownum), column(columnum))
+  end
+
+  def row(num)
+    raise "Invalid row number #{num}" unless (0..8).include?(num)
+    return @rows[num]
+  end
+
+  def column(num)
+    raise "Invalid column number #{num}" unless (0..8).include?(num)
+    return @columns[num]
+  end
+
+end
+
+
+class Board
+  class Position
+
+    def initialize(board, row, column)
+      @value = nil
+      @board = board
+      @row = row
+      @column = column
+    end
+
+    def assign(newval)
+      @value = newval
+      # remove old val?
+      @row.add(newval)
+      @column.add(newval)
+    end
+  end
+end
+
+class Board
+  class NumberSet
+    def initialize
+      @values = {}
+    end
+    def contains?(num)
+      return @values[num] == true
+    end
+    def add(num)
+      @values[num] = true
+    end
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -22,4 +22,24 @@ describe Board do
 
     expect(squares.size).to be 9
   end
+
+  it 'can assign a number to a position' do
+    board.position(4,1).assign(5)
+  end
+
+  context 'a number is assigned to a position' do
+    before do
+      @board = Board.new
+      @board.position(4,1).assign(5)
+    end
+    let(:board) { @board} 
+    it 'knows that the number is in that row' do
+      expect(board.row(4).contains?(5)).to be true
+      expect(board.row(4).contains?(7)).to be false
+    end
+    it 'knows that the number is in that column' do
+      expect(board.column(1).contains?(5)).to be true
+      expect(board.column(1).contains?(7)).to be false
+    end
+  end
 end
